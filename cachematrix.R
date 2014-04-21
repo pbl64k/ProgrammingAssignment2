7903e5f5aaf8353fc141833cb7d4b17b3eecd85c
pbl64k <pbl64k@gmail.com>
2014-04-21 19:32:42 +0300

    Typos.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## A simple implementation of memoized matrix inverse.
 
 ## Constructs a compound data structure consisting of a matrix together with
-## its cached inverse. Returns a list providing function to retrieve/alter
+## its cached inverse. Returns a list providing functions to retrieve/alter
 ## the state of the data structure.
 ##
 ## Note that doing it this way is not necessarily a good idea, as the innards
@@ -9,9 +9,10 @@
 ## the cached inverse does correspond to the stored matrix.
 makeCacheMatrix <- function(x = matrix()) {
     x.inv <- NULL
+    
     set <- function(x.new) {
-        # Binds the x in the enclosing function scope to the new matrix;
-        # resets the cached inverse.
+        ## Binds the x parameter  in the enclosing function's scope to the new
+        ## matrix; resets the cached inverse.
         x <<- x.new
         x.inv <<- NULL
     }
@@ -24,21 +25,20 @@ makeCacheMatrix <- function(x = matrix()) {
     get.inv <- function () {
         x.inv
     }
+    
     list(set = set, get = get, set.inv = set.inv, get.inv = get.inv)
 }
 
 
 ## Computes the inverse of a matrix, caching the result, and using it when
-## already available. Excepts the matrix in the representation created by
-## makeCacheMatrix()
+## already available. Expects the matrix in the representation created by
+## makeCacheMatrix() -- will break on other inputs
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
-    
-    # Compute the inverse if cached inverse is not present, and cache it.
+    ## Compute the inverse if cached inverse is not present, and cache it.
     if (is.null(x$get.inv())) {        
         x$set.inv(solve(x$get(), ...))
     }
     
-    # Returns the cached inverse.
+    ## Returns the cached inverse.
     x$get.inv()
 }
